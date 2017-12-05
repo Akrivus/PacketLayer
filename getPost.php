c0e8a328a4f6d8887a9003cbf91013a9d4336e76
Owen Adrian <akrivus@gmail.com>
2017-12-05 13:21:00 -0600

    Create getPost.php

---
+++ getPost.php
@@ -0,0 +1,17 @@
+ <?php
+ 
+$response = array();
+if($_SERVER['REQUEST_METHOD']=='GET') {
+  $id = $_GET['i'];
+  require_once 'DbOperation.php';
+  $db = new DbOperation();
+  $response = $db->getPost($id);
+  if ($response[1]->num_rows > 0) {
+    while ($row = $response[1]->fetch_assoc()) {
+	     echo json_encode($row);
+	   }
+  }
+  else {
+    echo "[]";
+  }
+}

4555ed5f6f58ff8a6a70c6f2443ddc7a9f57aedf
TheWizardDoctor <nshep25@gmail.com>
2017-12-20 16:26:20 -0600

    edited the server references out

--- config.php
+++ config.php
@@ -6,8 +6,8 @@
  * Time: 7:58 PM
  */
  
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'NCAPSSQLison#1');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'PM_PacketLayerDB');
+define('DB_USERNAME', /*INSERT A STRING OF THE SERVERS USERNAME*/);
+define('DB_PASSWORD', /*INSERT A STRING OF THE SERVERS PASSWORD*/);
+define('DB_HOST', /*INSERT A STRING OF THE SERVERS ADDRESS (you should use localhost if this is running on your server)*/);
+define('DB_NAME', /*INSERT A STRING OF THE DATABASE'S NAME*/);
 
